e7632a122f9e2d82578248da14e37c6aff85b4f6
unknown <maxime.cloarec@yahoo.fr>
2024-07-31 16:55:52 +0200

    Cookie et connexion

--- controller/controllerLogin.php
+++ controller/controllerLogin.php
@@ -1,9 +1,11 @@
 <?php include "function.php";
 include "model/modellogin.php" ?>
 
-
+<?php
+$data = loginForm();
+?>
 <?php echo viewHeader();
-echo viewLogin(loginForm());
+echo viewLogin($data);
 echo viewFooter();
 ?>
 
@@ -14,18 +16,24 @@ function loginForm()
         if (!isset($_POST["username"]) || empty($_POST["username"]) || !isset($_POST["password"]) || empty($_POST["password"])) {
             return "Les champs de connexion ne sont pas correctement rempli";
         }
-        $login = sanitize($_POST["login"]);
+        $login = sanitize($_POST["username"]);
         $password = sanitize($_POST["password"]);
         $bdd = connectionBDD();
         $verif = loginVerification($bdd, $login);
-        if (!isset($verif) || empty($verif)) {
+        if (empty($verif)) {
             return "L'identifiant de connexion n'existe pas";
         }
         $hash = userPassword($bdd, $login);
-        if (!password_verify($password, $hash)) {
+        if (!password_verify($password, $hash["password_users"])) {
             return "Le mot de passe rentré est incorrect";
         }
-        $_SESSION["data"] = getInfoLogin($bdd, $login);
+        // $_SESSION["data"] = getInfoLogin($bdd, $login);
+        if (isset($_POST["rememberMe"])) {
+            $token = bin2hex(random_bytes(64));
+            $bdd = connectionBDD();
+            updateToken($bdd, $token, $login);
+            setcookie("rememberMe",$token,time()+86400*30,"/","",false,true);
+        }
     }
 }
 ?>
--- model/modellogin.php
+++ model/modellogin.php
@@ -1,22 +1,34 @@
-<?php function loginVerification($bdd,$login){
+<?php function loginVerification($bdd, $login)
+{
     $req = $bdd->prepare("SELECT login_users FROM users WHERE login_users=?");
     $req->bindParam(1, $login, PDO::PARAM_STR);
     $req->execute();
     return $req->fetch(PDO::FETCH_ASSOC);
-}?>
+} ?>
 
-<?php function userPassword($bdd,$login) {
-$req = $bdd->prepare("SELECT mdp_users FROM users WHERE login_users=?");
-$req->bindParam(1, $login,PDO::PARAM_STR);
-$req->execute();
-return $req->fetchAll(PDO::FETCH_ASSOC);
-} 
+<?php function userPassword($bdd, $login)
+{
+    $req = $bdd->prepare("SELECT password_users FROM users WHERE login_users=?");
+    $req->bindParam(1, $login, PDO::PARAM_STR);
+    $req->execute();
+    return $req->fetch(PDO::FETCH_ASSOC);
+}
 ?>
 
-<?php function getInfoLogin($bdd,$login){
+<?php function getInfoLogin($bdd, $login)
+{
     $req = $bdd->prepare("SELECT id_users,login_users,numberstreet_users,city_users,postal_users,country_users FROM users WHERE login_users=?");
     $req->bindParam(1, $login, PDO::PARAM_STR);
     $req->execute();
     return $req->fetch(PDO::FETCH_ASSOC);
+}
+?>
+
+<?php function updateToken($bdd, $token, $login_user)
+{
+    $req = $bdd->prepare("UPDATE users SET rememberMe_users=? WHERE login_users= ? ");
+    $req->bindParam(1, $token, PDO::PARAM_STR);
+    $req->bindParam(2, $login_user, PDO::PARAM_STR);
+    $req->execute();
 }
 ?>
